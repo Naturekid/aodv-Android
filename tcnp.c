6f141228de40087ff83f09fe1a5abdc3b9755615
Naturekid <caibyyxf@sina.cn>
2015-04-08 11:21:31 +0800

    add tcnp.c .h to manage network topoloy change notification packet

--- tcnp.c
+++ tcnp.c
@@ -2,6 +2,10 @@
  Created by Cai Bingying- Version: Sun Dec 21 2014
  ***************************************************************************/
 #include "tcnp.h"
+#include "aodv.h"
+
+extern u_int8_t g_aodv_gateway;
+extern u_int32_t g_null_ip;
 
 //gen route redirection packet
 int gen_tcnp(u_int32_t src_ip,u_int32_t dst_ip,u_int32_t last_hop,unsigned char tos){
@@ -13,9 +17,9 @@ int gen_tcnp(u_int32_t src_ip,u_int32_t dst_ip,u_int32_t last_hop,unsigned char 
 	strcpy(dst,inet_ntoa(dst_ip));
     printk("----------------in gen tcnp(%s   %s)----------------\n",src,dst);
 #endif
-	tcnp *tmp_tcnp;
+	tcnp *tmp_tcnp=(tcnp *)kmalloc(sizeof(tcnp), GFP_ATOMIC);
 	
-	if ((tmp_tcnp = (tcnp *) kmalloc(sizeof(tcnp), GFP_ATOMIC)) == NULL) {
+	if(tmp_tcnp == NULL) {
 #ifdef DEBUG2
 		printk("Can't allocate new tcnp\n");
 #endif
@@ -30,8 +34,29 @@ int gen_tcnp(u_int32_t src_ip,u_int32_t dst_ip,u_int32_t last_hop,unsigned char 
 	tmp_tcnp->dst_ip = dst_ip;
 	tmp_tcnp->src_ip = src_ip;
 	tmp_tcnp->tos = tos;
+	
+	printk("----Last hop:%s in gen_tcnp\n",inet_ntoa(last_hop));
+	aodv_neigh* tmp_neigh = find_aodv_neigh(last_hop);
 
-	send_message(last_hop, NET_DIAMETER, tmp_tcnp,sizeof(tcnp),NULL);
+	if(tmp_neigh==NULL)
+	{
+#ifdef DEBUG2
+		printk("The lasthop is not a exist neigh in gen_tcnp.\n");
+#endif
+		kfree(tmp_tcnp);
+		return 0;
+	}
+	aodv_dev *lastdev = get_netdev_by_name(tmp_neigh->dev->name);
+
+	if(lastdev==NULL)
+	{
+#ifdef DEBUG2
+		printk("The lasthop dev is not exist in gen_tcnp.\n");
+#endif
+		kfree(tmp_tcnp);
+		return 0;
+	}
+	send_message(last_hop, NET_DIAMETER, tmp_tcnp,sizeof(tcnp),lastdev);
 	kfree(tmp_tcnp);
 					
 	return 1;
@@ -83,13 +108,12 @@ int recv_tcnp(task *tmp_packet){
 			printk("------------I am the tcnp source-----------");
 #endif
 			task *new_task;
-			new_task = create_task(TASK_GEN_RRRP);
-			new_task->src_ip = source;
-			new_task->dst_ip = destination;
-			new_task->tos = ip->tos;
+			new_task = create_task(TASK_GEN_RRREQ);
+			new_task->src_ip = tmp_route->src_ip;
+			new_task->dst_ip = tmp_route->dst_ip;
+			new_task->tos = tmp_route->tos;
 			insert_task_at_front(new_task);
-				
-#endif
+
 		    	kfree(tmp_tcnp);
 		}//the source
 
--- tcnp.h
+++ tcnp.h
@@ -4,10 +4,9 @@
 #ifndef TCNP_H
 #define TCNP_H
 
-
 #include "aodv.h"
 
-//gen route redirection packet
+//gen route rediscovery packet
 int gen_tcnp(u_int32_t src_ip,u_int32_t dst_ip,u_int32_t last_hop,unsigned char tos);
 int recv_tcnp(task *tmp_packet);
 
